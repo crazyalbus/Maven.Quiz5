9ed66352d6a4b1d37ca7708aa0cebb185c56b908
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-04-09 11:08:10 -0400

    powerSetofTwo all tests pass

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -1,17 +1,33 @@
 package rocks.zipcode.io.quiz4.generics;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class GenericUtils {
     public static <SomeType extends Comparable> Iterable<? extends Iterable<SomeType>> powerSet(Set<SomeType> originalSet) {
+        List<SomeType> originalList = new ArrayList<>(originalSet);
+        List<List<SomeType>> powerList = new ArrayList<>();
 
+        int powerSetSize = (int) Math.pow(2, originalSet.size());
 
-        return null;
+        int i = powerSetSize - 1;
+        while (i >= 0) {
+            List<SomeType> temp = new ArrayList<>();
+            String pattern = Integer.toBinaryString(i);
+            if(pattern.length() < Integer.toBinaryString(powerSetSize-1).length()) {
+                pattern = "0" + pattern;
+            }
+            for (int j = 0; j < pattern.length(); j++) {
+                if(Character.compare(pattern.charAt(j), '1') == 0) {
+                    temp.add(originalList.get(j));
+                }
+            }
+            powerList.add(temp);
+            i--;
+        }
+        return powerList;
     }
 
     public static <SomeType extends Comparable> Iterable<? extends Iterable<SomeType>> powerSet(SomeType... originalSet) {
