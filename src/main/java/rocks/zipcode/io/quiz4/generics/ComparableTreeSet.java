5ecec07bf6c36de3b32fbb852dd1e5609e849de1
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-04-09 11:27:26 -0400

    treeset polymorphism tests passed

--- src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
@@ -1,17 +1,24 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.TreeSet;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class    ComparableTreeSet<_> {
-    public ComparableTreeSet(_... arr) {
+public class    ComparableTreeSet<SomeType> extends TreeSet<SomeType> implements Comparable<SomeType> {
+    public ComparableTreeSet(SomeType... arr) {
     }
 
 
     public ComparableTreeSet() {
     }
 
-    public int compareTo(ComparableTreeSet<_> o) {
+    public int compareTo(ComparableTreeSet<SomeType> o) {
         return Integer.valueOf(null);
+    }
+
+    @Override
+    public int compareTo(SomeType o) {
+        return 0;
     }
 }
