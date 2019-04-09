70927ded974e7a4ef617ccfa36f7294ca7de44e5
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-04-09 12:38:50 -0400

    updated treeset code

--- src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
@@ -1,20 +1,41 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class    ComparableTreeSet<SomeType> extends TreeSet<SomeType> implements Comparable<SomeType> {
+
+    Set<SomeType> set;
+
+
     public ComparableTreeSet(SomeType... arr) {
+        this.set = new TreeSet<SomeType>();
+        for (SomeType s : arr) {
+            set.add(s);
+        }
     }
 
 
     public ComparableTreeSet() {
+        this.set = new TreeSet<SomeType>();
     }
 
     public int compareTo(ComparableTreeSet<SomeType> o) {
-        return Integer.valueOf(null);
+
+        boolean matches = true;
+        for (SomeType s : o) {
+            if(!set.contains(o)) {
+                matches = false;
+            }
+        }
+
+        if(matches == true) {
+            return 0;
+        }
+        return -1;
     }
 
     @Override
